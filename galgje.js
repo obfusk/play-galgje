87de76f3025ba9453643d76cdb20dd05a7845b25
Felix C. Stegerman <flx@obfusk.net>
2012-03-20 01:00:01 +0100

    ~ galgje.css (font + pad.); ~ galgje.html (giveup + fmt); ~ galgje.js (done; word: @end, random; * -> _; giveup)
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- galgje.js
+++ galgje.js
@@ -17,6 +17,8 @@ var max_bad       = 11;
 var word          = null;
 var letters       = null;
 var guessed       = null;
+
+var done          = null;
 var n_bad         = null;
 var n_good        = null;
 
@@ -26,29 +28,28 @@ function set_gallows (n) {
   $('#gallows').attr ('src', 'img/' + n + '.svg');
 }
 
-function set_word (xs) {
-  $('#word').html (xs.join (' '));
-}
-
+function set_word     (s) { $('#word'   ).html (s); }
 function set_guessed  (s) { $('#guessed').html (s); }
 function set_info     (s) { $('#info'   ).html (s); }
 
 //  --
 
 function init () {                                            //  {{{1
-  word    = "konijn";                                         //  TODO
+  word    = words[Math.floor (Math.random () * words.length)];
 
   letters = [];
   guessed = {};
+
+  done    = false;
   n_bad   = 0;
   n_good  = 0;
 
   for (var i in word) {
-    letters[i] = '*';
+    letters[i] = '_';
   }
 
   set_gallows (0);
-  set_word    (letters);
+  set_word    (letters.join (' '));
   set_guessed (guessed_text);
   set_info    (info_text);
 }                                                             //  }}}1
@@ -70,16 +71,17 @@ function add_letter (c) {                                     //  {{{1
 
 function good_letter (n) {                                    //  {{{1
   n_good += n;
-  set_word (letters);
 
   if (n_good == word.length) {
     $('body').addClass ('won');
+
+    set_word (word);
     set_info (won_text);
 
-    return true;
+    done = true;
   }
   else {
-    return false;
+    set_word (letters.join (' '));
   }
 }                                                             //  }}}1
 
@@ -89,24 +91,29 @@ function bad_letter () {                                      //  {{{1
 
   if (n_bad == max_bad) {
     $('body').addClass ('lost');
+
+    set_word (word);
     set_info (lost_text);
 
-    return true;
-  }
-  else {
-    return false;
+    done = true;
   }
 }                                                             //  }}}1
 
 function guess_letter (c) {                                   //  {{{1
-  if (n_bad < max_bad && n_good < word.length) {
+  if (! done) {
     if (guessed[c]) {
       set_info (c + already_text);
     }
     else {
       guessed[c]  = true;
       var n       = add_letter (c);
-      var done    = n ? good_letter (n) : bad_letter ();
+
+      if (n) {
+        good_letter (n)
+      }
+      else {
+        bad_letter ();
+      }
 
       if (! done) {
         set_info (info_text);
@@ -120,6 +127,13 @@ function guess_letter (c) {                                   //  {{{1
 //  --
 
 function new_click (e) { location.reload (); }
+
+function giveup_click (e) {                                   //  {{{1
+  if (! done) {
+    n_bad = max_bad - 1;
+    bad_letter ();
+  }
+}                                                             //  }}}1
 
 function body_keypress (e) {                                  //  {{{1
   var k = e.keyCode;
@@ -136,8 +150,9 @@ function body_keypress (e) {                                  //  {{{1
 function on_ready () {                                        //  {{{1
   init ();
 
-  $('#new').click     (new_click);
-  $('body').keypress  (body_keypress);
+  $('#new'    ).click     (new_click);
+  $('#giveup' ).click     (giveup_click);
+  $('body'    ).keypress  (body_keypress);
 }                                                             //  }}}1
 
 //  --
